9aad6c6d9c498defad76d4b6905b8d5fed4fbee6
alessiamarcolini <98marcolini@gmail.com>
2016-06-30 18:29:32 +0200

    MCC Implementation

--- mcc_multiclass.py
+++ mcc_multiclass.py
@@ -5,13 +5,13 @@ import numpy as np
 def multimcc(t,p, classes=None):
     """ Matthews Correlation Coefficient for multiclass
     :Parameters:
-        t : 1d array_like object integer 
+        t : 1d array_like object integer
           target values
-        p : 1d array_like object integer 
+        p : 1d array_like object integer
           predicted values
-        classes: 1d array_like object integer containing 
+        classes: 1d array_like object integer containing
           all possible classes
-    
+
     :Returns:
         MCC : float, in range [-1.0, 1.0]
     """
@@ -19,15 +19,15 @@ def multimcc(t,p, classes=None):
     # Cast to integer
     tarr = np.asarray(t, dtype=np.int)
     parr = np.asarray(p, dtype=np.int)
-    
-    
+
+
     # Get the classes
     if classes is None:
         classes = np.unique(tarr)
-    
+
     nt = tarr.shape[0]
     nc = classes.shape[0]
-    
+
     # Check dimension of the two array
     if tarr.shape[0] != parr.shape[0]:
         raise ValueError("t, p: shape mismatch")
@@ -36,7 +36,7 @@ def multimcc(t,p, classes=None):
     X = np.zeros((nt, nc))
     Y = np.zeros((nt, nc))
 
-    # Fill the matrices 
+    # Fill the matrices
     for i,c in enumerate(classes):
         yidx = np.where(tarr==c)
         xidx = np.where(parr==c)
@@ -47,7 +47,7 @@ def multimcc(t,p, classes=None):
     # Compute the denominator
     denom = cov(X,X) * cov(Y,Y)
     denom = np.sqrt(denom)
-    
+
     if denom == 0:
         # If all samples assigned to one class return 0
         return 0
@@ -63,7 +63,7 @@ def confusion_matrix(t, p):
           target values
         p : 1d array_like object integer (-1/+1)
           predicted values
-    
+
     :Returns:
         MCC : float, in range [-1.0, 1.0]
     """
@@ -71,7 +71,7 @@ def confusion_matrix(t, p):
     # Read true and predicted classes
     tarr = np.asarray(t, dtype=np.int)
     parr = np.asarray(p, dtype=np.int)
-    
+
     # Get the classes
     classes = np.unique(tarr)
 
@@ -97,31 +97,31 @@ def confusion_matrix(t, p):
 
 def cov(x,y):
     nt = x.shape[0]
-    
+
     xm, ym = x.mean(axis=0), y.mean(axis=0)
     xxm = x - xm
     yym = y - ym
-    
+
     tmp = np.sum(xxm * yym, axis=1)
     ss = tmp.sum()
 
     return ss/nt
 
-    
+
 if __name__ == '__main__':
 
     import numpy as np
     ytrue = np.array([ 7.,  6.,  2.,  2.,  2.,  2.,  6.,  2.,  2.,  3.,
                        2.,  7.,  6., 2.,  5.,  7.,  2.,  2.,  2.])
-    
+
     ypred = np.array([7,  3,  6,  2,  2,  2,  3,  6,  2,
                       3,  2,  5,  3,  2,  2,  3,  2, 2,  2])
-    
+
     ypred = np.copy(ytrue)
-    ypred[0] = 6.0 
-    
+    ypred[0] = 6.0
+
     # ytrue = np.array([9., 2., 1., 2., 1., 5., 3., 9.,10., 4., 8., 5., 5., 3., 2., 9., 4., 3., 7., 5., 7., 9.,10., 7., 4., 1., 4., 1., 6., 6.,1., 9., 4., 4., 3., 6., 5., 3., 1., 1., 7.,10., 6., 1., 8., 1., 9., 1., 5., 4., 2., 1., 2., 8., 2., 1., 3., 7., 5., 5.,1., 4.,10., 8., 5., 7., 6., 2., 1., 3., 4.,10., 2., 4., 6.,6., 8.,10., 1., 5., 4., 2., 8., 1., 7., 1.,10., 9., 3., 1.,2., 3., 1., 2., 6., 3., 1., 6., 3., 3., 6., 1., 1., 2., 1.,2., 6., 2., 1., 8., 6., 5., 7., 1.,10., 4.,10., 8., 7.,10.,8., 3., 2., 9., 3., 3., 2., 2., 8., 7., 5., 9., 6., 2., 2.,4., 1., 2., 1., 5., 4., 1., 4., 4., 1., 4., 4., 1., 2., 5.,2., 1., 6., 2., 1., 9., 4., 1., 2., 2., 6., 4., 1., 2., 4.,1., 1., 4., 4., 2., 2., 2., 8., 3., 1., 1., 8., 2., 1., 6.,7., 1., 2., 8.,10., 1., 2., 4., 4.,10., 7., 1., 5., 7., 8.,5., 4., 1., 1., 2.,10., 2.])
 
     # ypred = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-    print multimcc(ytrue, ypred)
+    print(multimcc(ytrue, ypred))
