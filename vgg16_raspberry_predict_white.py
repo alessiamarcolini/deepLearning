d08a63bce43d37ef4cf97fb61536aab7305ed9d8
alessiamarcolini <98marcolini@gmail.com>
2016-07-02 21:40:19 +0200

    bugfix

--- vgg16_raspberry_predict_white.py
+++ vgg16_raspberry_predict_white.py
@@ -229,7 +229,7 @@ plt.plot(perc_white, mcc_list, "-", color="red")
 plt.plot(perc_white, mcc_list, "o", color="blue")
 plt.ylabel("MCC")
 plt.xlabel("White")
-fig = pyplot.figure()
+fig = plt.figure()
 ax = fig.add_subplot(111)
 for i,j in zip(perc_white,mcc_list):
     ax.annotate(str(j),xy=(i,j+0.5))
