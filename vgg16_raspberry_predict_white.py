6f9063b0374306ce8842d6d0fe1e73f8a2b7d7eb
alessiamarcolini <98marcolini@gmail.com>
2016-07-02 17:24:19 +0200

    Bugfix

--- vgg16_raspberry_predict_white.py
+++ vgg16_raspberry_predict_white.py
@@ -11,7 +11,7 @@ from keras.optimizers import SGD
 from os.path import join, getsize
 import sys
 from mcc_multiclass import multimcc, confusion_matrix
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 def load_im2(paths):
