429f9a47fa32c7b4e5b94ac80dc14d465860a059
alessiamarcolini <98marcolini@gmail.com>
2016-07-02 18:43:41 +0200

    Improved image generator

--- img_to_white.py
+++ img_to_white.py
@@ -4,34 +4,39 @@ import os
 
 white=[255,255,255]
 n=90  #le righe bianche che aggiunge (2 sopra, 2 sotto, 2 sx, 2 dx)
-directory = "early"
 
-path = 'BerrySamples_Original/'
-paths = os.listdir(path + "/" + directory)
-paths.sort()
-
-for numb, name in enumerate(paths):
+numbers = range(1,16)
+directories = ["early", "good", "late"]
 
 
+for num in numbers:
+    for directory in directories:
+        path = 'BerrySamples_Original/'
+        paths = os.listdir(path + "/" + directory)
+        paths.sort()
 
-  img = Image.open(path + directory + "/" + name )
-  arr = np.array(img)
-  size=arr.shape
-
-  whiterow=[white]*(size[1]+n*2)
-
-  matrix=[whiterow]*n
-
-
-  for i in range(size[0]):
-  	line=(([white]*n)+(arr[i].tolist())+([white]*n))
-  	matrix.append(line)
-
-  for i in range(n):
-  	matrix.append(whiterow)
+        for numb, name in enumerate(paths):
 
 
-  newarr=np.array(matrix, dtype=np.uint8)
 
-  image = Image.fromarray(newarr,'RGB')
-  image.save("BerrySamples_White/" + directory + "/" + directory + "_W_" + str(n) + "_"+ str(numb+1) + ".jpg" )
+          img = Image.open(path + directory + "/" + name )
+          arr = np.array(img)
+          size=arr.shape
+
+          whiterow=[white]*(size[1]+n*2)
+
+          matrix=[whiterow]*n
+
+
+          for i in range(size[0]):
+          	line=(([white]*n)+(arr[i].tolist())+([white]*n))
+          	matrix.append(line)
+
+          for i in range(n):
+          	matrix.append(whiterow)
+
+
+          newarr=np.array(matrix, dtype=np.uint8)
+
+          image = Image.fromarray(newarr,'RGB')
+          image.save("BerrySamples_White/" + directory + "/" + directory + "_W_" + str(n) + "_"+ str(numb+1) + ".jpg" )
