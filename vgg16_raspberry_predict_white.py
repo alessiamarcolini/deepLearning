28d08562a3a359a184c1faf59a4357972afd32ed
alessiamarcolini <98marcolini@gmail.com>
2016-07-02 17:33:21 +0200

    bugfix

--- vgg16_raspberry_predict_white.py
+++ vgg16_raspberry_predict_white.py
@@ -214,11 +214,11 @@ for p in perc:
 mcc_list = np.array(mcc_list)
 perc_white = np.array(perc_white)
 
-plt.pyplot.bar(mcc_list, perc_white)
+plt.bar(mcc_list, perc_white)
 
 
 for x,y in zip(perc_white, mcc_list):
-    plt.pyplot.text(x+0.4, y+0.05, '%.4f' % y, ha='center', va= 'bottom')
+    plt.text(x+0.4, y+0.05, '%.4f' % y, ha='center', va= 'bottom')
 
-plt.pyplot.savefig("plot.png")
+plt.savefig("plot.png")
 #plt.show()
