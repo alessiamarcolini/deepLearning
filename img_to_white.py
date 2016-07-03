a0f8b9f0b4c022e122bc00b16be443b60a881f25
alessiamarcolini <98marcolini@gmail.com>
2016-07-03 22:46:36 +0200

    Changed path

--- img_to_white.py
+++ img_to_white.py
@@ -2,10 +2,10 @@ from PIL import Image
 import numpy as np
 import os
 
-white=[255,255,255]
+white=[104,125,165]
 #n=90  #le righe bianche che aggiunge (2 sopra, 2 sotto, 2 sx, 2 dx)
 
-numbers = range(1,16)
+numbers = [0,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,32,64,90,128,256,512,1024,2048]
 directories = ["early", "good", "late"]
 
 
@@ -39,4 +39,4 @@ for n in numbers:
           newarr=np.array(matrix, dtype=np.uint8)
 
           image = Image.fromarray(newarr,'RGB')
-          image.save("BerrySamples_White/" + directory + "/" + directory + "_W_" + str(n) + "_"+ str(numb+1) + ".jpg" )
+          image.save("BerrySamples_Blue/" + directory + "/" + directory + "_B_" + str(n) + "_"+ str(numb+1) + ".jpg" )
