1fff4950b9df24b218088d5aab7fafc60146682d
alessiamarcolini <98marcolini@gmail.com>
2016-07-05 02:14:49 +0200

    Improved code structure and parameterization of datasets

--- vgg16_raspberry_predict.py
+++ vgg16_raspberry_predict.py
@@ -27,165 +27,210 @@ def load_im2(paths):
     return l
 
 
-# path to the model weights files.
-weights_path = 'vgg16_first_training_raspberry_weights.h5'
+def dataset_to_parameters(dataset):
+    validation_data_dir = ("/".join(["datasets", dataset.lower()]))
+    if len(os.listdir(validation_data_dir)) == 3:
+        predict_mcc = True
+    else:
+        predict_mcc= False
 
-# dimensions of our images.
-img_width, img_height = 224, 224
+    return predict_mcc
 
+def vgg16(weights_path=None, add_fully_connected=True):
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
 
-validation_data_dir = 'BerrySamples_Original'
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-# build the VGG16 network
-model = Sequential()
-model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Flatten(input_shape=model.output_shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    fully_connected()
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+def fully_connected():
 
-model.add(Flatten(input_shape=model.output_shape[1:]))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(3, activation='sigmoid'))
-
-# load the weights of the VGG16 networks
-# (trained on ImageNet, won the ILSVRC competition in 2014)
-# note: when there is a complete match between your model definition
-# and your weight savefile, you can simply call model.load_weights(filename)
-
-assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
-model.load_weights(weights_path)
-print('Model loaded.')
-
-# build a classifier model to put on top of the convolutional model
-
-# note that it is necessary to start with a fully-trained
-# classifier, including the top classifier,
-# in order to successfully do fine-tuning
-#top_model.load_weights(top_model_weights_path)
-
-
-# set the first 25 layers (up to the last conv block)
-# to non-trainable (weights will not be updated)
-for layer in model.layers[:25]:
-    layer.trainable = False
-
-# compile the model with a SGD/momentum optimizer
-# and a very slow learning rate.
-model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-              metrics=['accuracy'])
-
-
-validation_images = []
-validation_labels = []
-val_path_e = validation_data_dir + "/early/"
-val_path_g = validation_data_dir + "/good/"
-val_path_l = validation_data_dir + "/late/"
-val_paths = [val_path_e, val_path_g, val_path_l]
-
-val_filenames_e = os.listdir(val_path_e)
-val_filenames_g = os.listdir(val_path_g)
-val_filenames_l = os.listdir(val_path_l)
-
-for path in val_paths:
-    if path == val_path_e:
-        for name in val_filenames_e:
-            validation_images.append(path + name)
-            validation_labels.append([1,0,0])
-    elif path == val_path_g:
-        for name in val_filenames_g:
-            validation_images.append(path + name)
-            validation_labels.append([0,1,0])
-    elif path == val_path_l:
-        for name in val_filenames_l:
-            validation_images.append(path + name)
-            validation_labels.append([0,0,1])
+    model.add(Dense(3, activation='sigmoid'))
 
 
 
-validation = np.array(load_im2(validation_images))
+def create_validationImg_validationLabel_list(predict_mcc):
+
+    if predict_mcc:
+        validation_images = []
+        validation_labels = []
+        val_path_e = validation_data_dir + "/early/"
+        val_path_g = validation_data_dir + "/good/"
+        val_path_l = validation_data_dir + "/late/"
+        val_paths = [val_path_e, val_path_g, val_path_l]
+
+        val_filenames_e = os.listdir(val_path_e)
+        val_filenames_g = os.listdir(val_path_g)
+        val_filenames_l = os.listdir(val_path_l)
+
+        for path in val_paths:
+            if path == val_path_e:
+                for name in val_filenames_e:
+                    validation_images.append(path + name)
+                    validation_labels.append([1,0,0])
+            elif path == val_path_g:
+                for name in val_filenames_g:
+                    validation_images.append(path + name)
+                    validation_labels.append([0,1,0])
+            elif path == val_path_l:
+                for name in val_filenames_l:
+                    validation_images.append(path + name)
+                    validation_labels.append([0,0,1])
+
+def file_generator(predict_mcc):
+
+    lines = []
+    predicted_labels_linear = []
+    validation_labels_linear = []
+
+    for i in range(len(predicted_labels)):
+        cls_prob = predicted_labels[i]
+
+        predicted_labels_linear.append(np.argmax(cls_prob))
+
+        if predicted_labels_linear[i] == 0:
+            predicted_label = "Early"
+        elif predicted_labels_linear[i] == 1:
+            predicted_label = "Good"
+        elif predicted_labels_linear[i] == 2:
+            predicted_label = "Late"
+
+        line = [validation_images[i], predicted_label, str(round(cls_prob[0],3)),
+                str(round(cls_prob[1],3)), str(round(cls_prob[2],3))]
+
+        if predict_mcc:
+
+            for j in range(len(validation_labels[i])):
+                cl = validation_labels[i][j]
+                validation_labels_linear.append(j)
+                if cl == 1 and j == 0:
+                    real_label = "Early"
+
+                elif  cl == 1 and j == 1:
+                    real_label = "Good"
+
+                elif  cl == 1 and j == 2:
+                    real_label = "Late"
+
+            line.append(real_label)
+
+        lines.append(";".join(line)+"\n")
 
 
+    validation_labels_linear = np.array(validation_labels_linear)
+    predicted_labels_linear = np.array(predicted_labels_linear)
+
+    return lines, validation_labels_linear, predicted_labels_linear
 
 
-predicted_labels = model.predict(validation)
+def MCC_CM_calculator(validation_labels_linear, predicted_labels_linear):
+    '''Return MCC and confusion matrix'''
 
-prediction_summary = open("vgg16_first_train_raspberry_prediction_summary.csv", "w")
-prediction_summary.write("\t".join(['FILENAME', 'REAL_LABEL', 'PREDICTED_LABELS'])+'\n')
+    MCC = multimcc(validation_labels_linear, predicted_labels_linear)
+    MCC_line = "MCC=" + str(MCC) + "\n"
 
-predicted_labels_linear = []
+    CM_line = str(confusion_matrix(validation_labels_linear, predicted_labels_linear))
 
-for i in range(len(predicted_labels)):
-    cls_prob = predicted_labels[i]
-    for j in range(len(validation_labels[i])):
-        cl = validation_labels[i][j]
-        if cl == 1 and j == 0:
-            real_label = "Early"
-
-        elif  cl == 1 and j == 1:
-            real_label = "Good"
-
-        elif  cl == 1 and j == 2:
-            real_label = "Late"
-
-    line = [validation_images[i], real_label, str(round(cls_prob[0],3)), str(round(cls_prob[1],3)), str(round(cls_prob[2],3))]
-    predicted_labels_linear.append(np.argmax(cls_prob))
-    prediction_summary.write(";".join(line)+"\n")
-    prediction_summary.flush()
+    return MCC_line, CM_line
 
 
+def main():
+    file_lines = []
+    dataset = sys.argv[0]
+    # path to the model weights files.
+    weights_path = 'vgg16_first_training_raspberry_weights.h5'
 
-validation_labels_linear = []
+    # dimensions of our images.
+    img_width, img_height = 224, 224
+    predict_mcc = dataset_to_parameters(dataset)
 
-for lbl in validation_labels:
-    if lbl[0] == 1:
-        validation_labels_linear.append(0)
-    if lbl[1] == 1:
-        validation_labels_linear.append(1)
-    if lbl[2] == 1:
-        validation_labels_linear.append(2)
+    # load the weights of the VGG16 networks
+    # (trained on ImageNet, won the ILSVRC competition in 2014)
+    # note: when there is a complete match between your model definition
+    # and your weight savefile, you can simply call model.load_weights(filename
+    vgg16(weights_path)
 
-validation_labels_linear = np.array(validation_labels_linear)
-predicted_labels_linear = np.array(predicted_labels_linear)
+    assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
+    model.load_weights(weights_path)
+    print('Model loaded.')
 
-MCC = multimcc(validation_labels_linear, predicted_labels_linear)
-prediction_summary.write("MCC=" + str(MCC) + "\n")
-prediction_summary.flush()
+    # build a classifier model to put on top of the convolutional model
 
-prediction_summary.write(str(confusion_matrix(validation_labels_linear, predicted_labels_linear)))
-prediction_summary.flush()
-prediction_summary.close()
+    # note that it is necessary to start with a fully-trained
+    # classifier, including the top classifier,
+    # in order to successfully do fine-tuning
+    #top_model.load_weights(top_model_weights_path)
+
+
+    # set the first 25 layers (up to the last conv block)
+    # to non-trainable (weights will not be updated)
+    for layer in model.layers[:25]:
+        layer.trainable = False
+
+    # compile the model with a SGD/momentum optimizer
+    # and a very slow learning rate.
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  metrics=['accuracy'])
+
+
+    create_validationImg_validationLabel_list(predict_mcc)
+
+    validation = np.array(load_im2(validation_images))
+    predicted_labels = model.predict(validation)
+
+    prediction_summary = open("vgg16_first_train_raspberry_prediction_summary_{}.csv".format(dataset), "w")
+    prediction_summary.write("\t".join(['FILENAME', 'PREDICTED_LABEL', 'PREDICTED_LABELS', 'REAL_LABEL'])+'\n')
+
+    if predict_mcc:
+        MCC_line, CM_line = MCC_CM_calculator(validation_labels_linear, predicted_labels_linear)
+        file_lines.append(MCC_line)
+        file_lines.append(CM_line)
+
+    lines, validation_labels_linear, predicted_labels_linear = file_generator(predict_mcc)
+    file_lines.extend(lines)
+
+
+    prediction_summary.close()
+
+
+if __name__ == "__main__":
+    main()
